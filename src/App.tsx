63728ef4ecbc03500dd9a1296a5d79aaa0f67d4c
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-09-20 17:24:02 -0300

    feat: import data context provider to app

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import React from "react";
 import Sidenav from "./layout/Sidenav";
 import Header from "./layout/Header";
 import Resume from "./pages/Resume";
+import { DataContextProvider } from "./context/DataContext";
 
 function App() {
   return (
-    <div>
-      <Sidenav />
-      <main>
-        <Header />
-        <Resume />
-      </main>
-    </div>
+    <DataContextProvider>
+      <div>
+        <Sidenav />
+        <main>
+          <Header />
+          <Resume />
+        </main>
+      </div>
+    </DataContextProvider>
   );
 }
 
