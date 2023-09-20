a5bd7e9bc7eb69a5860384f061ef1738134090b5
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-09-20 16:55:34 -0300

    feat: import basic layout components for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Sidenav from "./layout/Sidenav";
+import Header from "./layout/Header";
+import Resume from "./pages/Resume";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Sidenav />
+      <main>
+        <Header />
+        <Resume />
+      </main>
+    </div>
+  );
 }
 
 export default App;
