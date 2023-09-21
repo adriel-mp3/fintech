a7e34f18798c9a230edb0d3d1cfd2e57300fb8da
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-09-21 10:40:03 -0300

    feat: add container style to app

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { DataContextProvider } from "./context/DataContext";
 function App() {
   return (
     <DataContextProvider>
-      <div>
+      <div className="container">
         <Sidenav />
         <main>
           <Header />
