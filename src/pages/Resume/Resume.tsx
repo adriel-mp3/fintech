df73c7cc91764af88f2e2283820323717fbce684
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-09-21 16:26:49 -0300

    feat: add resume info

---
+++ src/pages/Resume/Resume.tsx
@@ -0,0 +1,43 @@
+import styles from "./Resume.module.css";
+import { useData } from "../../context/DataContext";
+
+const Resume = () => {
+  const { data } = useData();
+  
+  if (!data) return null;
+  return (
+    <section>
+      <div className={`${styles.resume} flex mb`}>
+        <div className="box">
+          <h2>Vendas</h2>
+          <span>
+            {data
+              .filter((item) => item.status === "falha")
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("pt-br", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+        <div className="box">
+          <h2>Recebido</h2>
+          <span>
+            {data
+              .filter((item) => item.status === "pago")
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("pt-br", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+        <div className="box">
+          <h2>Processando</h2>
+          <span>
+            {data
+              .filter((item) => item.status === "processando")
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("pt-br", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Resume;
