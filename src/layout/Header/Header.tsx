b5f85832dc9070aabf467b3f4e96fa1f15d3d5a2
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-09-22 14:55:26 -0300

    feat: add reactive title with hook useLocation from react-router-dom

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,11 +1,22 @@
 import React from "react";
-import styles from './Header.module.css';
+import styles from "./Header.module.css";
 import DateRange from "../../components/DateRange/DateRange";
 import MonthOptions from "../../components/MonthOptions/MonthOptions";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
-  const [title, setTitle] = React.useState("Resumo");
-
+  const [title, setTitle] = React.useState("");
+  const { pathname } = useLocation();
+  React.useEffect(() => {
+    if (pathname === "/") {
+      setTitle("Resumo");
+      document.title = " Fintech | Resumo";
+    } else {
+      document.title = " Fintech | Vendas";
+      setTitle("Vendas");
+    }
+  }, [pathname]);
+  
   return (
     <header className="mb">
       <div className={`${styles.daterange} mb `}>
